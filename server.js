059beaa0be03eddb6928a2e9b2d10c82fd188b8e
Benaka R N <benaka27112003@gmail.com>
2026-05-05 04:25:35 +0000

    add welcome route to the API

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ connectDB();
 
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the Household Inventory API");
+});
 const authMiddleware = require("./middleware/authMiddleware");
 
 
